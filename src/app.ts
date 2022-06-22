696558cb62a32fdb946f869800c6441f021bb984
João Lourenço <joaodslourenco@gmail.com>
2022-06-22 09:25:58 -0300

    feat: database connection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import db from "./config/dbConnect";
+
+db.on("error", () => console.log("Erro ao conectar com o banco de dados."));
+db.once("open", () => console.log("Banco de dados conectado com sucesso."));
 
 const app = express();
 
